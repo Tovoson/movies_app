7d32416106b6dd829f3cfeed371a09d74ec3092a
Ely <elytovoniaina@gmail.com>
2025-03-26 06:56:26 +0300

    tabs fini

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#030014',
+        secondary: '#151312',
+        light:{
+          100: "#D6C6FF",
+          100: "#A8B5DB",
+          100: "#9CA4AB",
+        },
+        dark:{
+          100: "#221f3d",
+          100: "#0f0d23",
+        },
+        accent: '#AB8BFF',
+      }
+    },
   },
   plugins: [],
 }
